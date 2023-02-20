4041b77e20660446dc916aa053f91c2cd8911df0
Gargi Singh <gargi_singh@icloud.com>
2023-02-20 23:51:48 +0530

    20/02

---
+++ Arr6.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Arr6 {
+    public static ArrayList < Integer > separateNegativeAndPositive(ArrayList < Integer > nums) {
+
+        int j = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums.get(i) < 0) {
+
+               
+                if (i != j) {
+                    int temp = nums.get(i);
+                    nums.set(i, nums.get(j));
+                    nums.set(j, temp);
+                }
+
+                ++j;
+            }
+        }
+
+        return nums;
+    }
+    
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>(n);
+        for(int i = 0; i < n; i++){
+                arr.add(i);
+        }
+        System.out.println(separateNegativeAndPositive(arr));
+    }
+} 
